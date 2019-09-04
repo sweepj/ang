99ae52a088ea13e212c67dfd881061ca42d5526b
sweepj <n0thing.super@yandex.ua>
2019-09-04 12:14:40 +0300

    brackets phone number v1.1

--- src/app/custom-form/directives/custom-input.directive.ts
+++ src/app/custom-form/directives/custom-input.directive.ts
@@ -9,36 +9,30 @@ export class CustomInputDirective {
   @Input('numbers') numbers: string;
 
   private regex = {
-    number: new RegExp(/[+]\d\s\d\s|\d+$/)
+    number: new RegExp(/^[+]\d\s\d\s|\d+$/)
   };
 
   private specialKeys = {
     number: [ 'Backspace', 'Tab', 'End', 'Home', 'ArrowLeft', 'ArrowRight']
   };
 
+  private defaultCharBrackets = 2;
   constructor(private ElemRef: ElementRef, private render: Renderer2, private customFormCom: CustomFormComponent) {}
-  temp = [];
 
   @HostListener('keydown', ['$event']) onKeyDown(event) {
-    // if (event.target.value.length === this.customFormCom.findCountry.length) {
-    //   event.target.value = event.target.value + ' ';
-    // }
     if (this.specialKeys[this.numbers].indexOf(event.key) !== -1) {
       return;
     }
-    if (event.target.value.length === 5) {
+    if (event.target.value.length === (this.customFormCom.findCountry.length + this.defaultCharBrackets)) {
       let valueInput = event.target.value.split('');
-      valueInput = valueInput.splice(2, 6);
+      valueInput = valueInput.splice(this.customFormCom.findCountry.length, this.customFormCom.findCountry.length +
+        this.defaultCharBrackets);
       valueInput.unshift(' (');
       valueInput.push(') ');
       valueInput = valueInput.join('');
       event.target.value = this.customFormCom.findCountry + valueInput;
-      // event.target.value = valueInput + this.temp.join('');
-    }   else {
-      // this.temp = [];
     }
-    this.temp.push(event.key);
-    console.log(this.temp);
+    console.log(event);
     const current: string = this.ElemRef.nativeElement.value;
     const next: string = current.concat(event.key);
     if (next && !String(next).match(this.regex[this.numbers])) {
