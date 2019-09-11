185078de348ceef4d175ded79156459cfbbea27d
sweepj <n0thing.super@yandex.ua>
2019-09-11 17:39:57 +0300

    delet number in input

--- src/app/custom-form/directives/custom-input.directive.ts
+++ src/app/custom-form/directives/custom-input.directive.ts
@@ -8,10 +8,6 @@ export class CustomInputDirective {
 
   @Input('numbers') numbers: string;
 
-  // private regex = {
-  //   number: new RegExp(/^\d+$/)
-  // };
-
   private specialKeys = {
     number: ['Backspace', 'Tab', 'End', 'Home', 'ArrowLeft', 'ArrowRight']
   };
@@ -26,39 +22,43 @@ export class CustomInputDirective {
 
     if (event.key === 'Backspace') {
       let positionCaret = this.ElemRef.nativeElement.selectionEnd;
-      positionCaret--;
-      if ((event.target.value[positionCaret] === ' ') && ((positionCaret+1) !== event.target.value.length)){
+      let tempValueInput = event.target.value.split('');
+      if ((event.target.value[(positionCaret-1)] === ' ') && ((positionCaret+1) !== event.target.value.length)){
         event.preventDefault();
         this.ElemRef.nativeElement.selectionEnd--;
+      } else if(((positionCaret+1) !== event.target.value.length) && (event.target.value[positionCaret] !== ' ')) {
+        let tempLength = tempValueInput.length-1;
+        for (let z = tempLength; z > positionCaret; z--){
+          debugger
+          let y = z;
+          tempValueInput[z] = tempValueInput[--y];
+          console.log(tempValueInput);
+        }
+        event.preventDefault();
       }
-
     }
-
+    this.valueInput = event.target.value;
     if (this.specialKeys[this.numbers].indexOf(event.key) !== -1) {
       return;
     }
 
-
     if((/\D/.test(event.key)) || (event.target.value.length === 15)){
-      console.log(event.target.value.length);
       event.preventDefault();
     }
+
     let replaceVal = this.customFormCom.replaceVal;
     let mask = this.customFormCom.regexp;
-    this.valueInput = event.target.value;
-    this.maskInput(mask,replaceVal);
+    this.maskInput(mask,replaceVal,event);
     event.target.value = this.valueInput;
   }
 
 
-  maskInput(mask,replaceVal) {
+  maskInput(mask,replaceVal,e) {
     for (let i = 0; i < mask.length; i++) {
       if (this.valueInput.match(RegExp(mask[i]))) {
         this.valueInput = this.valueInput.replace(RegExp(mask[i]), replaceVal[i]);
       }
     }
   }
-
-
 
 }
